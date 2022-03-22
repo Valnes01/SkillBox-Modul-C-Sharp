20a04c6ed4bad21cd587f60a78a09582a33d3940
Ivan Fedchenko <paladin7701@gmail.com>
2022-03-22 19:18:37 +0200

    ДЗ 4
    Удалил дз 3, вставил дз 4. Задача 1 и 2

--- SkillBox Modul C Sharp/Program.cs
+++ SkillBox Modul C Sharp/Program.cs
@@ -9,120 +9,65 @@ namespace SkillBox_Modul_C_Sharp
 {
     class Program
     {
+        
         static void Main()
         {
-            //    //задача 3.1. 
-            Console.WriteLine("Сейчас мы проверим четное или нечетное Ваше число \nВведите число для проверки");
-            int number1 = Convert.ToInt32(Console.ReadLine());
-            if (number1 % 2 == 0)
+            //Задача 4.1
+            Console.WriteLine("Введите количество строк в матрице:");
+            int lines = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов в матрице:");
+            int columns = Convert.ToInt32(Console.ReadLine());
+            int[,] matrix = new int[lines, columns];
+            Random num = new Random();
+            int sum = 0;
+            for (int i = 0; i < lines; i++)
             {
-                Console.WriteLine($"Число {number1} - четное");
-            }
-            else
-            {
-                Console.WriteLine($"Число {number1} - нечетное");
-            }
-            while (true)
-            {
-                Console.WriteLine("Хотите проверить еще число? Нажмите 1 - если да, 2 - нет");
-                int sequel = Convert.ToInt32(Console.ReadLine());
 
-                if (sequel == 1)
+                for (int j = 0; j < columns; j++)
                 {
-                    Console.WriteLine("Введите число для проверки");
-                    int number2 = Convert.ToInt32(Console.ReadLine());
-                    if (number2 % 2 == 0)
-                    {
-                        Console.WriteLine($"Число {number2} - четное");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Число {number2} - нечетное");
-                    }
+                    matrix[i, j] = num.Next(10);
+                    Console.Write($"{matrix[i, j]}\t");
+                    sum += matrix[i, j];
                 }
-                else
-                {
-                    Console.WriteLine("Досвидания!");
-                    Console.ReadKey();
-                    break;
-                }
-            }
+                Console.WriteLine();
 
-            //    //Задача 3.2
-            Console.Clear();
-            Console.WriteLine("Приветствую новый пользователь \nСейчас мы будем играть в \"Blackjack\" " +
-                "\nВ игре существуют следующие номиналы карт  цифры 1 до 10\n" +
-                    $"Для «картинок» следует принять обозначения латинскими буквами:" +
-                    $"\nВалет = J или j\nДама = Q или q\nКороль = K или k\nТуз = T или t " +
-                    $"\nНоминал картинки равен 10" +
-                "\nКакое количество карт у Вас в руках?");
-            int numCards = Convert.ToInt32(Console.ReadLine());
-            int sumCards = 0;
-            for (int i = 1; i <= numCards; i++)
-            {
-                Console.WriteLine($"Введите номинал {i} карты:");
-                string valueCard = Console.ReadLine();
-                int valueCard2;
-                switch (valueCard)
-                {
-                    case "J":
-                        sumCards += 10;
-                        break;
-                    case "j":
-                        sumCards += 10;
-                        break;
-                    case "Q":
-                        sumCards += 10;
-                        break;
-                    case "q":
-                        sumCards += 10;
-                        break;
-                    case "K":
-                        sumCards += 10;
-                        break;
-                    case "k":
-                        sumCards += 10;
-                        break;
-                    case "T":
-                        sumCards += 10;
-                        break;
-                    case "t":
-                        sumCards += 10;
-                        break;
-                    default:
-                        valueCard2 = Convert.ToInt32(valueCard);
-                        sumCards += valueCard2;
-                        break;
-                }
             }
-            Console.WriteLine($"Сумма Ваших карт {sumCards}");
+            Console.WriteLine($"Сумма элементов = {sum}");
             Console.ReadKey();
 
-
-            //Задача 3.3
+            //Задача 4.2
             Console.Clear();
-            Console.WriteLine("Здравствуйте! Сейчас мы проверим ваше число на простоту\n" +
-                "Простое число — число, которое делится само на себя и на единицу" +
-                "\nВведите любое число");
-            int number = Convert.ToInt32(Console.ReadLine()); //5
-            int j = 1;
-            int result;
-            bool ez;
-            do
+
+            Console.WriteLine("Введите размер последовательности");
+            int lenght = Convert.ToInt32(Console.ReadLine());
+            int p = 0;
+            int[] mass = new int[lenght];
+
+            for (int k = 0; k < lenght; ++k)
             {
-                j++;
-                result = number % j;
-                ez = result != 0;
+                Console.WriteLine($"Введите {++p} значение");
+
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                mass[k] = num1;
+
             }
-            while (j < number - 1 && ez == true);
-            if (ez == true)
+            int min = int.MaxValue;
+            for (int i = 0; i < lenght; i++)
             {
-                Console.WriteLine($"Число {number}  простое!");
+
+                if (min > mass[i])
+                {
+                    min = mass[i];
+                }
             }
-            else
+
+            Console.WriteLine($"Ваш массив выглядит следующим образом:");
+            foreach (int o in mass)
             {
-                Console.WriteLine($"Число {number} не простое!");
+                Console.Write($"{o}\t");
             }
+            Console.WriteLine($"\nНаименьшее число {min}");
+
             Console.ReadKey();
         }
             
