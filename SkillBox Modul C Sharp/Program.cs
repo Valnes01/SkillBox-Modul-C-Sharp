93096ff5fb589d8930c424c459b6f66046f4e750
Ivan Fedchenko <paladin7701@gmail.com>
2022-05-31 11:21:14 +0200

    Дз5
    Мелкие исправления

--- SkillBox Modul C Sharp/Program.cs
+++ SkillBox Modul C Sharp/Program.cs
@@ -12,18 +12,18 @@ namespace SkillBox_Modul_C_Sharp
         static void Main()
         {
             Console.WriteLine("Введите предложние:");
-            string Text = Console.ReadLine();
+            string text = Console.ReadLine();
             Console.WriteLine("Задача №1");
-            string[] separation = GetSeparationText(Text);
+            string[] separation = GetSeparationText(text);
             GetEnumerationText(separation);
             Console.ReadKey();
             Console.WriteLine("Задача №2");
-            GetInvertText(separation, Text);
+            GetInvertText(separation, text);
             Console.ReadKey();
         }
-        static string[] GetSeparationText(string Text) //метод для разделения строки на массив слов
+        static string[] GetSeparationText(string text) //метод для разделения строки на массив слов
         {
-            string[] separation = Text.Split(' ');
+            string[] separation = text.Split(' ');
             return separation;
         }
         static void GetEnumerationText(string [] separationA) //метод выведения на консоль массива слов
@@ -33,9 +33,9 @@ namespace SkillBox_Modul_C_Sharp
                 Console.WriteLine(separationA[i]);
             }
         }
-        static void GetInvertText(string [] separationA, string Text) //метод инверсии
+        static void GetInvertText(string [] separationA, string text) //метод инверсии
         {
-            GetSeparationText(Text);
+            GetSeparationText(text);
             for (int i = 0, j = separationA.Length-1; i < j; i++, j--)
             {
                 string A = separationA[i];
@@ -48,8 +48,8 @@ namespace SkillBox_Modul_C_Sharp
             }
             Console.WriteLine();
             Console.WriteLine("Объединение в строку массива слов:");
-            var NewString = string.Join(" ", separationA);
-            Console.WriteLine(NewString);  
+            var newString = string.Join(" ", separationA);
+            Console.WriteLine(newString);  
         }
     }
 }
